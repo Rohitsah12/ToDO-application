f7eb1a5fe3606146b08888bc8b3add3e850c2b4c
Rohit Kumar Sah <rohitkumar233179@gmail.com>
2024-12-18 18:41:49 +0530

    starting with events

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+
 console.log("Welcome to my todo app");
+
+let  getTodosButton=document.getElementById('get-todos');
+//Registration of an event listener
+getTodosButton.addEventListener("click",()=>{
+    console.log("clicked");   
+})
